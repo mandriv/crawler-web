903d8fc0c7f27dee39c248a0211128060681b4a5
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-19 14:24:42 +0100

    Use normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'normalize.css';
 import App from './App';
 import registerServiceWorker from './config/registerServiceWorker';
 // eslint-disable-next-line
