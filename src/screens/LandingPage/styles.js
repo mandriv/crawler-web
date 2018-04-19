bfb266119f806ed17969f1e5f381992f59506f16
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-19 14:47:55 +0100

    Modify background gradient

--- src/screens/LandingPage/styles.js
+++ src/screens/LandingPage/styles.js
@@ -1,6 +1,6 @@
 import glamorous from 'glamorous';
 
-import { primary, primaryLight, rgba, white, whiteSmoke } from '../../config/colours';
+import { primary, primaryLight, primaryDark, rgba, white, whiteSmoke } from '../../config/colours';
 
 export const VideoBackground = glamorous.video({
   width: '100%',
@@ -14,7 +14,7 @@ export const GradientOverlay = glamorous.div({
   position: 'absolute',
   width: '100%',
   height: '100vh',
-  background: `linear-gradient(to right, ${rgba(primaryLight, 0.4)}, ${rgba(primary, 0.4)})`,
+  background: `linear-gradient(to right, ${rgba(primary, 0.4)}, ${rgba(primaryDark, 0.4)})`,
 });
 
 export const Content = glamorous.div({
